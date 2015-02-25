cabceeefe2474c4dd49ca5759dc7956c9afb8c13
7mears <7mears@gmail.com>
2015-02-24 20:58:45 -0600

    initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Global Exteriors
+ */
+?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<p>Global Exteriors</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Global Exteriors
+ */
+
+get_header(); ?>
+
+<section class="hero">
+  <div class="wrapper">
+    <h1>Header 1</h1>
+    <p>Completely formulate high-quality catalysts for change vis-a-vis stand-alone manufactured products. Distinctively grow functional expertise with premium niche markets. Appropriately facilitate low-risk high-yield innovation with extensible strategic theme areas. Credibly coordinate flexible customer service after exceptional alignments. Distinctively optimize seamless experiences for technically sound value.
+
+Globally customize resource-leveling ideas and resource-leveling manufactured products. Enthusiastically matrix intuitive quality vectors for high standards in metrics. Authoritatively supply.</p>
+  <a href="#0"><div class="button">Button</div></a>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
