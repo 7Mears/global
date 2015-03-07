ecd32dfe3101a1638024e3db587fcd0626008ddb
7mears <7mears@gmail.com>
2015-03-07 16:10:48 -0600

    made mobile nav / header

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'global' ); ?></button>
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
