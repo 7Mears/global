57152c0f6457b3fa94fb5cff10ffd8811cdcee12
7Mears <7mears@gmail.com>
2015-03-15 18:27:31 -0600

    if header has custom field display it

--- full-page.php
+++ full-page.php
@@ -9,7 +9,15 @@ get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post();?>
   <div class="page--title">
     <div class="wrapper">
+
+    <?php if( !empty(get_field('full_page_header')) ): ?>
+      <h1 id="post-<?php the_ID(); ?>"><?php the_field('full_page_header'); ?></h1>
+    <?php else: ?>
+
       <h1 id="post-<?php the_ID(); ?>"><?php the_title();?></h1>
+
+    <?php endif; ?>
+
     </div><!-- /wrapper -->
   </div><!-- /page title -->
 
